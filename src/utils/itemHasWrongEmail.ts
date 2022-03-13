032d3bb3bb4ff7142a155cf1e9b00c5676ba1188
Kriauciunas <kriauciunaskarolis3@gmail.com>
2022-03-13 18:26:20 +0200

    Fixed wrong emails !

--- src/utils/itemHasWrongEmail.ts
+++ src/utils/itemHasWrongEmail.ts
@@ -3,7 +3,7 @@ import { IItem } from '~/services/getUserItems';
 const itemHasWrongEmail = (item: IItem) => {
   const validEmail =
     /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-  return validEmail.test(item.email);
+  return !validEmail.test(item.email);
 };
 
 export default itemHasWrongEmail;
