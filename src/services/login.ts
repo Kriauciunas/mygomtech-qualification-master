efffa66d189208e8afc4c93a70ff065aff14fbef
Kriauciunas <kriauciunaskarolis3@gmail.com>
2022-03-13 16:59:00 +0200

    displaying an incorrect password or username

--- src/services/login.ts
+++ src/services/login.ts
@@ -1,4 +1,4 @@
-import {API} from '~/constants';
+import { API } from '~/constants';
 import getUrl from '../utils/getUrl';
 
 const login = async (username: string, password: string) => {
@@ -6,12 +6,15 @@ const login = async (username: string, password: string) => {
     username,
     password,
   });
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    const { token } = data;
 
-  const response = await fetch(url);
-  const data = await response.json();
-  const { token } = data;
-
-  localStorage.setItem('token', token);
+    localStorage.setItem('token', token);
+  } catch (error) {
+    throw { message: 'Invalid password or username' };
+  }
 };
 
 export default login;
