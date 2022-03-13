b9f4843f17bec721a19a8a2641b215f79f8fbfd7
Kriauciunas <kriauciunaskarolis3@gmail.com>
2022-03-13 20:25:07 +0200

    <h1> wrong email

--- src/components/UsersManagement/components/Header/Header.tsx
+++ src/components/UsersManagement/components/Header/Header.tsx
@@ -1,7 +1,8 @@
-import {FC} from 'react';
-import {IItem} from "~/services/getUserItems";
+import { FC } from 'react';
+import { IItem } from '~/services/getUserItems';
 import logout from '../../../../services/logout';
 
+import itemHasWrongEmail from '~/utils/itemHasWrongEmail';
 import './header-style.scss';
 
 interface IHeader {
@@ -9,17 +10,20 @@ interface IHeader {
   username: string;
 }
 
-const Header: FC<IHeader> = ({items, username}) => {
+const Header: FC<IHeader> = ({ items, username }) => {
+  const wrongEmails = items.filter((item) => itemHasWrongEmail(item));
 
   return (
-    <div className="header">
-      <div className="user-section">
+    <div className='header'>
+      <div className='user-section'>
         <button onClick={logout}>{`Logout ${username}`}</button>
       </div>
-      <h1>{`${items.length} Emails are wrong`}</h1>
-      <span>Email validator to protect your company from bad registrations</span>
+      <h1>{`${wrongEmails.length} Emails are wrong`}</h1>
+      <span>
+        Email validator to protect your company from bad registrations
+      </span>
     </div>
-  )
+  );
 };
 
 export default Header;
